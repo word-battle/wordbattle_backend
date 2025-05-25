553aa9e661f6169bba163b2af2a3c88637569523
JYB\jybeo <jybeomss1@gmail.com>
2025-05-25 20:23:37 +0900

    init: project init

---
+++ src/main/java/com/jybeomss1/wordbattle_backend/WordbattleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jybeomss1.wordbattle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordbattleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordbattleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jybeomss1/wordbattle_backend/WordbattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jybeomss1.wordbattle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordbattleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
