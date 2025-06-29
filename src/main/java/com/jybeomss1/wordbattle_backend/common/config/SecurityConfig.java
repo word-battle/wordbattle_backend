b44f00dcf53ac5cc984a6e519df58e0bd930938b
JYB\jybeo <jybeomss1@gmail.com>
2025-06-29 16:50:02 +0900

    feature: cors 추가

--- src/main/java/com/jybeomss1/wordbattle_backend/common/config/SecurityConfig.java
+++ src/main/java/com/jybeomss1/wordbattle_backend/common/config/SecurityConfig.java
@@ -21,6 +21,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -31,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
