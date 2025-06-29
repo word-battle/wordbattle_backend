2afa7ebdcfeffba820e2864a797db59569d54946
JYB\jybeo <jybeomss1@gmail.com>
2025-06-29 16:54:34 +0900

    feature: cors 추가

--- src/main/java/com/jybeomss1/wordbattle_backend/common/config/WebConfig.java
+++ src/main/java/com/jybeomss1/wordbattle_backend/common/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.jybeomss1.wordbattle_backend.common.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
