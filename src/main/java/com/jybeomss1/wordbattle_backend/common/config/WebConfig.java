1078e52cc4beec981705dab74a1e9e44c10d47a3
JYB\jybeo <jybeomss1@gmail.com>
2025-07-06 20:03:24 +0900

    feature: webConfig 추가

---
+++ src/main/java/com/jybeomss1/wordbattle_backend/common/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.jybeomss1.wordbattle_backend.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origin-ip}")
+    private String allowedOriginIp;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOriginIp)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
