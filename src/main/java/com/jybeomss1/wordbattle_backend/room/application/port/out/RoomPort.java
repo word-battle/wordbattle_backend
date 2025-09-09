1aaa2c4f409c4dbe823d7ba291272230e9bf965d
JYB\jybeo <jybeomss1@gmail.com>
2025-09-09 22:08:20 +0900

    fix: room exit api 추가

--- src/main/java/com/jybeomss1/wordbattle_backend/room/application/port/out/RoomPort.java
+++ src/main/java/com/jybeomss1/wordbattle_backend/room/application/port/out/RoomPort.java
@@ -13,5 +13,4 @@ public interface RoomPort {
     Optional<Room> findById(UUID roomId);
     Page<Room> findRooms(GameStatus gameStatus, Pageable pageable);
     Optional<Room> findByJoinCode(String joinCode);
-    void exitRoom(UUID roomId, UUID userId);
 } 
